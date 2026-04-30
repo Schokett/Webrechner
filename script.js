8ccf250ddaad74320a70e197e75998ead07c645d
schokett <skrollanelenazimmermann@gmail.com>
2026-04-30 11:58:29 +0200

    handleOperator with actions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let secentDisplay = document.getElementById("secent-display");
+let secentCurrentCalculation = secentDisplay.innerText;
 let firstDisplay = 0;
 let textContainer = 0;
 let resultDisplay = document.getElementById("first-display");
@@ -21,14 +22,39 @@ function deleteButton() {
   resultDisplay.innerText = "0";
 }
 
-// funktion zum addieren
-function additionButton() {
-  if (!resultDisplay.innerText.endsWith("+")) {
-    resultDisplay.innerText += "+";
-    secentDisplay.innerText = resultDisplay.innerText;
+function handleOperator(op) {
+  const operators = ["+", "-", "*", "/"];
+  const currentFullNumber = resultDisplay.innerText;
+
+  const lastChar = resultDisplay.innerText.slice(-1);
+  if (resultDisplay.innerText === "" || resultDisplay.innerText === "0") return;
+  resultDisplay.innerText += op;
+
+  //nach dem operator klickt wird der result display zum secent überreicht
+  if (secentDisplay.innerText === "0" || secentDisplay.innerText === "") {
+    secentDisplay.innerText = currentFullNumber + op;
+    console.log(currentFullNumber);
   } else {
-    return;
+    secentDisplay.innerText += currentFullNumber + op;
   }
+  // resultdisplay wird gecleart und schickt den wert an secentDisplay
+  resultDisplay.innerText = "";
+  // secentCurrentCalculation.innerText = currentFullNumber + op;
+
+  // secentDisplay muss den letzten wert speichern den er hatt und einem neuen Wert empfangen können.
+
+  // secent display angezeigt werden.
+  // KONTROLLE : wenn man zb 2+2= macht soll 4 rauskommen wenn mann aber 2+2+= drückt soll 8 rauskommen da er "2+2=4" die 4+4 rechnet!
+  //gleich danach muss die Rechen aufgabe rechts angeigt werden (egal ob nach einen operator oder einem ist gleich)
 }
+function equalsButton() {
+  console.log("ich bin die = funktion");
+}
+console.log(secentCurrentCalculation);
+// *** --------------- AUFGABEN ---------------***
+// 1.
+// Operator funktion =
+
+// 2.
 // Sobald ein Operator geklickt wurden ist,
 // soll der wert im secentDisplay GESPIECHERT Werden
