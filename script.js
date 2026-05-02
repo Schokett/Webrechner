b778c5d07b1b4976dadd8f05642744a29705224e
schokett <skrollanelenazimmermann@gmail.com>
2026-05-02 03:42:21 +0200

    update Display funktionen

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 let secentDisplay = document.getElementById("secent-display");
 let resultDisplay = document.getElementById("first-display");
-let result = "";
+let result = 0;
 let calculationChain = "";
+let operatorsActive = false;
+let numbersActive = false;
 
 const operators = ["+", "-", "*", "/"];
 
-//Löscht alles auf resultDisplay
-function deleteButton() {
-  resultDisplay.innerText = "0";
-}
 // Verarbeitet das Tippen der Ziffern 0-9
 function appendNumber(number) {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = number;
-  } else {
-    resultDisplay.innerText += number;
-  }
+  result += number;
+  numbersActive = true;
+  console.log(result);
+  console.log(numbersActive);
+
+  updateNumbersResultDisplay(number);
 }
 
 // Verarbeitet +, -, *, /
 function handleOperator(op) {
-  if (
-    resultDisplay.innerText !== "0" &&
-    !operators.some((op) => resultDisplay.innerText.includes(op))
-  ) {
-    resultDisplay.innerText += op;
-  } else {
-    resultDisplay.innerText = resultDisplay.innerText.slice(0, -1) + op;
-  }
+  operatorsActive = true;
+  updateOperatorsResultDisplay(op);
 }
 
 // Die reine Mathe-Logik (Input -> Output)
 function calculate() {}
 
-// Bringt alles auf den Bildschirm (First & Secent Display)
-function updateDisplay() {}
-
 // Speziell für das Gleichheitszeichen
 function finalizeResult(equal) {
   // console.log(equal);
+}
+
+//Löscht alles auf resultDisplay
+function deleteButton() {
+  resultDisplay.innerText = 0;
+}
+
+// Bringt alles auf den Bildschirm (First & Secent Display) ↓
+
+// ANZEIGEN von Nummern
+function updateNumbersResultDisplay(numbers) {
+  if (numbersActive) {
+    if (resultDisplay.innerText === "0") {
+      resultDisplay.innerText = numbers;
+    } else {
+      resultDisplay.innerText += numbers;
+    }
+  }
+}
+// ANZEIGEN von Operatoren
+function updateOperatorsResultDisplay(op) {
+  if (!operators.some((el) => resultDisplay.innerText.endsWith(el))) {
+    resultDisplay.innerText += op;
+    console.log("hat ein operator hinzugefügt");
+  } else {
+    resultDisplay.innerText = resultDisplay.innerText.slice(0, -1) + op;
+    console.log("hat ein operator hinzugefügt aber auch den letzten gelöscht");
+  }
+
+  if (result === 0) {
+  }
 }
